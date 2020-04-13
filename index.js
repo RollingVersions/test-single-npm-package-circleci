e26481e39f2e971394397cf577af7943669d6f0d
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-13 18:38:00 +0100

    fix: make public

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const profile = require('npm-profile');
-const readConfig = require('libnpmconfig').read;
-
-profile.get({...readConfig().toJSON()}).then(res => console.log(res)).catch(ex=> {
-  console.error(ex.stack);
-  process.exit(1);
-});
+module.exports = 1;
